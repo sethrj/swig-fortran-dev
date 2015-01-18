00e7f0d2f98e0c9c658250c2e02829378dbed14f
Seth R Johnson <johnsonsr@ornl.gov>
2015-01-18 16:59:06 -0500

    Hypothetical updates

--- hypothetical_vec/hypothetical.f90
+++ hypothetical_vec/hypothetical.f90
@@ -1,8 +1,7 @@
 module vecmod_I
  use, intrinsic :: ISO_C_BINDING
  interface
-
-  function new_Vec_Dbl(farg1) &
+  function wrap_new_Vec_Dbl(farg1) &
     result(fresult) &
     bind(C, name="_wrap_new_Vec_Dbl")
    use, intrinsic :: ISO_C_BINDING
@@ -11,32 +10,31 @@ module vecmod_I
    integer(C_INT) :: farg1
   end function
 
-  subroutine delete_Vec_Dbl(this) &
+  subroutine wrap_delete_Vec_Dbl(thisptr) &
     bind(C, name="_wrap_delete_Vec_Dbl")
    use, intrinsic :: ISO_C_BINDING
    implicit none
-   type(C_PTR) :: this
+   type(C_PTR) :: thisptr
   end subroutine
 
-  function Vec_Dbl_at(this, farg1) &
+  function wrap_Vec_Dbl_at(thisptr, farg1) &
     result(fresult) &
     bind(C, name="_wrap_Vec_Dbl_at")
    use, intrinsic :: ISO_C_BINDING
    implicit none
    real(C_DOUBLE) :: fresult
-   type(C_PTR) :: this
+   type(C_PTR) :: thisptr
    integer(C_INT) :: farg1
   end function
-  
-  subroutine Vec_Dbl_set(this, farg1, farg2) &
+
+  subroutine wrap_Vec_Dbl_set(thisptr, farg1, farg2) &
     bind(C, name="_wrap_Vec_Dbl_set")
    use, intrinsic :: ISO_C_BINDING
    implicit none
-   type(C_PTR) :: this
+   type(C_PTR) :: thisptr
    integer(C_INT) :: farg1
    real(C_DOUBLE) :: farg2
   end subroutine
-  
  end interface
 end module
 
@@ -44,13 +42,15 @@ module vecmod
  use, intrinsic :: ISO_C_BINDING
  use vecmod_I
  implicit none
- 
+
  type Vec_Dbl
   type(C_PTR),private :: ptr = C_NULL_PTR
  contains
   ! DESTRUCTORS AND METHODS
-  procedure :: set
-  ! procedure :: at
+  procedure :: set => Vec_Dbl_set
+  procedure :: at => Vec_Dbl_at
+  procedure :: delete => delete_Vec_Dbl
+  ! final :: delete
   !  procedure :: get
   !  procedure :: set
   !  procedure :: size
@@ -58,54 +58,54 @@ module vecmod
 
  ! CONSTRUCTORS
  interface Vec_Dbl
-  procedure :: new
-  !final :: delete
-  ! procedure :: at
+  procedure :: new_Vec_Dbl
   !procedure :: construct_from_array
  end interface
 
 contains
-! subroutine at(this, v) &
-!   bind(C, name="wrap_Vec_Dbl_at")
-!  use,intrinsic :: ISO_C_BINDING
-!  implicit none
-!  integer(C_INT) :: farg1
-!  return _wrap_Vec_Dbl_at(v)
-! end subroutine
+ ! subroutine at(this, v) &
+ !   bind(C, name="wrap_Vec_Dbl_at")
+ !  use,intrinsic :: ISO_C_BINDING
+ !  implicit none
+ !  integer(C_INT) :: farg1
+ !  return _wrap_Vec_Dbl_at(v)
+ ! end subroutine
 
  ! Constructors
- function new(farg1) &
+ function new_Vec_Dbl(farg1) &
    result(this)
   type(Vec_Dbl) :: this
   integer(C_INT) :: farg1
-  this%ptr = new_Vec_Dbl(farg1)
+  this%ptr = wrap_new_Vec_Dbl(farg1)
  end function
 
- subroutine set(this, farg1, farg2)
+ subroutine Vec_Dbl_set(this, farg1, farg2)
   use,intrinsic :: ISO_C_BINDING
   implicit none
   class(Vec_Dbl) :: this
   integer(C_INT) :: farg1
   real(C_DOUBLE) :: farg2
-  call Vec_Dbl_set(this%ptr, farg1, farg2)
+  call wrap_Vec_Dbl_set(this%ptr, farg1, farg2)
  end subroutine
 
- function at(this, farg1) &
+ function Vec_Dbl_at(this, farg1) &
    result(fresult)
   use,intrinsic :: ISO_C_BINDING
   implicit none
   class(Vec_Dbl) :: this
   integer(C_INT) :: farg1
   real(C_DOUBLE) :: fresult
-  fresult = Vec_Dbl_at(this%ptr, farg1)
+  fresult = wrap_Vec_Dbl_at(this%ptr, farg1)
  end function
 
  ! Destructor
- subroutine delete(this)
-  type(Vec_Dbl) :: this
-  call delete_Vec_Dbl(this%ptr)
+ subroutine delete_Vec_Dbl(this)
+  use,intrinsic :: ISO_C_BINDING
+  implicit none
+  class(Vec_Dbl) :: this
+  call wrap_delete_Vec_Dbl(this%ptr)
+  this%ptr = C_NULL_PTR
  end subroutine
-
 end module
 
 program foo
