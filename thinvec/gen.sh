c4da817ebbcebb64276bceafc91a3ba1335dac9f
Seth R Johnson <johnsonsr@ornl.gov>
2016-11-14 15:28:07 -0500

    Regenerate SWIG with 3.0.11

--- thinvec/gen.sh
+++ thinvec/gen.sh
@@ -1,20 +1,22 @@
-#!/bin/bash
+#!/bin/bash -ex
 ###############################################################################
 # File  : thinvec/gen.sh
 # Author: Seth R Johnson
 # Date  : Sat Jan 17 00:01:17 2015
 ###############################################################################
 
+# Note: even though you're running from teh build directory, SWIG requries the
+# .swg files to be installed
 swig=/Users/s3j/_code/_build/swig-debug/swig
 
 #for lang in python fortran java
 for lang in fortran
 do
   dirname=swig-${lang}
-  mkdir "${dirname}" 2>/dev/null
-  cd ${dirname} || exit $?
+  mkdir "${dirname}"  2>/dev/null || true
+  cd ${dirname}
   echo "Calling swig with ${lang}"
-  ln -s ../vector.* . 2>/dev/null
+  ln -s ../vector.* . 2>/dev/null || true
   for n in 4
   do
     flags="-debug-module ${n} -debug-typemap -debug-tmsearch -debug-tmused"
