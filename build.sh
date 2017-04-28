0e4547e92be9084d296a3055dc1658dea87a3c53
Seth R Johnson <johnsonsr@ornl.gov>
2017-04-28 15:18:35 -0400

    Updating for cmake change

--- build.sh
+++ build.sh
@@ -9,33 +9,16 @@ BUILD=$HOME/_code/_build
 SOURCE=$HOME/_code/swig/Examples/fortran
 GEN=$HOME/_code/swig-fortran-dev/generated
 
-cd $BUILD
-for d in \
-  spdemo \
-  algorithm \
-  barefunctions \
-  exceptions \
-  inheritance \
-  simpleclass \
-  std_string \
-  std_vector \
-  thinvec
-do
-  builddir="$BUILD/devswig-$d"
-  mkdir $builddir 2>/dev/null || true
-  cd $builddir
-  if [ -f CMakeCache.txt ]
-  then
-    make clean
-  else
-    cmake -C $SOURCE/cmake/config.cmake $SOURCE/$d
-  fi
-  make || true
-  ./test.exe
-  dstdir="$GEN/$d"
-  mkdir 2>/dev/null || true
-  cp *.cxx *.f90 $dstdir
-done
+builddir="$BUILD/devswig-examples"
+mkdir $builddir 2>/dev/null || true
+cd $builddir
+if [ -f CMakeCache.txt ]
+then
+  make clean
+else
+  cmake -C $SOURCE/cmake/config.cmake $SOURCE
+fi
+make && ctest -V
 
 ###############################################################################
 # end of swig-fortran-dev/build.sh
