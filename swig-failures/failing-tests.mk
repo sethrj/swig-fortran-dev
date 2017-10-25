372c617f2f562bd75242fdb88bfd0ed3f6510980
Seth R Johnson <johnsonsr@ornl.gov>
2017-10-25 17:39:50 -0400

    Reprocess

--- swig-failures/failing-tests.mk
+++ swig-failures/failing-tests.mk
@@ -1,36 +1,29 @@
 
 # failed to compile fortran
-allprotected \
+abstract_signature \
   autodoc \
-  constant_pointers \
   constructor_copy \
-  default_args \
-  default_constructor \
+  cpp_nodefault \
   director_basic \
+  director_classes \
+  director_default \
   director_detect \
-  director_enum \
   director_frob \
   director_ignore \
   director_keywords \
   director_nested \
-  director_overload \
+  director_overload2 \
   director_property \
   director_protected \
   director_redefined \
-  enum_ignore \
   enum_scope_template \
-  enum_thorough \
   evil_diamond_prop \
-  extend_default \
-  extend_template_method \
   extern_c \
   friends \
-  global_namespace \
-  li_std_vector_enum \
-  mixed_types \
-  multiple_inheritance_abstract \
-  multiple_inheritance_interfaces \
-  multiple_inheritance_nspace \
+  fvirtual \
+  inherit_target_language \
+  kwargs_feature \
+  multiple_inheritance \
   namespace_class \
   namespace_nested \
   naturalvar_more \
@@ -38,39 +31,39 @@ allprotected \
   nested_workaround \
   null_pointer \
   operator_overload \
+  operator_overload_break \
   overload_arrays \
   overload_method \
   overload_polymorphic \
-  overload_return_type \
   overload_simple \
   overload_subtype \
-  overload_template \
-  overload_template_fast \
   preproc_constants \
   primitive_ref \
   refcount \
-  return_const_value \
   samename \
   smart_pointer_const \
+  smart_pointer_const2 \
   smart_pointer_extend \
+  smart_pointer_ignore \
   smart_pointer_inherit \
   smart_pointer_member \
   smart_pointer_multi \
   smart_pointer_multi_typedef \
+  smart_pointer_namespace \
+  smart_pointer_namespace2 \
+  smart_pointer_not \
   smart_pointer_overload \
   smart_pointer_protected \
   smart_pointer_rename \
   smart_pointer_simple \
+  smart_pointer_static \
   smart_pointer_template_const_overload \
   smart_pointer_template_defaults_overload \
+  smart_pointer_templatemethods \
   smart_pointer_templatevariables \
   smart_pointer_typedef \
   special_variables \
-  template_arg_typename \
-  template_basic \
-  template_default_arg \
   template_enum \
-  template_explicit \
   template_extend_overload \
   template_methods \
   template_ns \
@@ -78,26 +71,53 @@ allprotected \
   template_specialization_enum \
   template_typedef_inherit \
   typemap_ns_using \
+  typemap_out_optimal \
   types_directive \
   using1 \
   using2 \
+  using_composition \
+  using_extend \
+  using_inherit \
   using_protected \
   valuewrapper_base \
-  valuewrapper_const \
   valuewrapper_opaque \
+  variable_replacement \
   wallkw
 
 # failed to compile wrapper
-apply_strings \
+aggregate \
+  allprotected \
+  apply_strings \
+  argout \
+  array_typedef_memberin \
+  arrayref \
+  arrays_global \
+  arrays_global_twodim \
+  catches \
+  char_binary \
+  char_strings \
+  constant_pointers \
+  constover \
   contract \
   cpp_basic \
   cpp_enum \
+  default_args \
+  default_constructor \
   director_binary_string \
+  director_enum \
   director_extend \
+  director_primitives \
   director_smartptr \
+  director_string \
   dynamic_cast \
+  enum_ignore \
+  enum_thorough \
+  exception_order \
   extend \
+  extend_default \
+  extend_placement \
   funcptr_cpp \
+  global_namespace \
   global_vars \
   li_attribute \
   li_attribute_template \
@@ -105,27 +125,52 @@ apply_strings \
   li_boost_shared_ptr_bits \
   li_boost_shared_ptr_template \
   li_cdata_cpp \
-  li_std_combinations \
   li_std_except \
   li_std_except_as_class \
+  li_std_string \
   li_std_vector \
+  li_std_vector_enum \
   li_std_vector_ptr \
+  li_swigtype_inout \
   member_funcptr_galore \
   member_pointer \
   member_pointer_const \
   memberin_extend \
+  mixed_types \
+  multiple_inheritance_abstract \
+  multiple_inheritance_interfaces \
+  multiple_inheritance_nspace \
   multiple_inheritance_shared_ptr \
   name_warnings \
   namespace_spaces \
   namespace_typemap \
+  nested_ignore \
   operator_pointer_ref \
+  overload_bool \
+  overload_return_type \
+  overload_template \
+  overload_template_fast \
+  pointer_reference \
+  return_const_value \
   smart_pointer_const_overload \
   special_variable_macros \
+  string_constants \
+  swig_exception \
+  template_arg_typename \
+  template_basic \
+  template_const_ref \
+  template_default_arg \
+  template_default_arg_overloaded \
+  template_explicit \
+  template_nested_typemaps \
   template_rename \
   template_typedef_rec \
+  threads \
+  threads_exception \
   typedef_sizet \
   typedef_struct_cpp \
   typemap_various \
+  valuewrapper_const \
   varargs \
   varargs_overload \
   virtual_poly \
