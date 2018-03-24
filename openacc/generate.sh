b2e1e6b8bf1e23e10fe1ac1cb36acb45314e9204
Seth R Johnson <johnsonsr@ornl.gov>
2018-03-24 17:54:46 -0400

    Update generating script

--- openacc/generate.sh
+++ openacc/generate.sh
@@ -1,8 +1,9 @@
 #!/bin/sh -ex
 
-CUDAROOT="/Developer/NVIDIA/CUDA-9.1"
 SWIG="/Users/s3j/_code/_build/swig-debug/swig"
 SWIG_LIB_DIR="/Users/s3j/_code/swig/Lib"
 
 export SWIG_LIB_DIR
 ${SWIG} -c++ -fortran thrustacc.i
+# The thrust calls have embedded kernel launches and device code
+mv thrustacc_wrap.cxx thrustacc_wrap.cu
